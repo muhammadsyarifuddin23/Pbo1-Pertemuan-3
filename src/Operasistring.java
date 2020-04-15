dd6d86d1f33a8c4047ce10c865c19fff22df43fb
muhammadsyarifuddin23 <beldadina987@gmail.com>
2020-04-15 23:59:28 +0800

    Initial commit

---
+++ src/Operasistring.java
@@ -0,0 +1,10 @@
+public class Operasistring {
+    public static void main(String[] args) {
+        String kota = "banjarbaru";
+
+        System.out.println(kota);
+        char[] uniskaChar = { 'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+    }
+}
